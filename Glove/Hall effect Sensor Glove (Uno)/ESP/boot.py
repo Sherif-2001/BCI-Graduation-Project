e66c312c89eba283966d73d188d4a27a99ba8fcd
Neveen Mohamed Ayman <94166833+NeveenMohamed@users.noreply.github.com>
2024-08-13 23:54:47 +0300

    Delete Glove/Hall effect Sensor Glove (Uno) directory

--- Glove/Hall effect Sensor Glove (Uno)/ESP/boot.py
+++
@@ -1,5 +0,0 @@
-import uasyncio as asyncio
-from schedule import schedule
-        
-asyncio.run(schedule())
-    
--- Glove/Hall effect Sensor Glove (Uno)/ESP/schedule.py
+++
@@ -1,8 +0,0 @@
-import uasyncio as asyncio
-from tasks import *
-
-async def schedule():
-    sensor_task = asyncio.create_task(read_sensors())
-    espnow_task = asyncio.create_task(send_espnow())
-    tcp_task = asyncio.create_task(send_tcp())
-    await asyncio.gather(sensor_task, tcp_task, espnow_task)
